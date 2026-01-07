31cc3b8eb357375180bd6c10c61ea94ec1b15d74
Saiake <65845952+Saiake@users.noreply.github.com>
2026-01-07 22:12:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-console.log('!!! LOADED !!! I AM ALIVE !!!');
-toastr.success('РАСШИРЕНИЕ ЗАГРУЖЕНО!');
+(function () {
+    const { SlashCommandParser, SlashCommand } = SillyTavern.getContext().slashCommand;
+
+    const switchProfileCommand = SlashCommand.fromProps({
+        name: 'switch',
+        callback: async (namedArgs, unnamedArgs) => {
+            const args = unnamedArgs.join(' ').trim();
+
+            if (!args) {
+                toastr.warning('Укажите номер профиля! Пример: /switch 2');
+                return;
+            }
+
+            try {
+                await fetch('http://localhost:9000/hooks/switch?id=' + args);
+                toastr.success('Переключаюсь на профиль ' + args);
+            } catch (e) {
+                toastr.error('Ошибка: ' + e.message);
+            }
+        },
+        helpString: 'Переключает на указанный профиль. Пример: /switch 2',
+    });
+
+    SlashCommandParser.addCommandObject(switchProfileCommand);
+})();
